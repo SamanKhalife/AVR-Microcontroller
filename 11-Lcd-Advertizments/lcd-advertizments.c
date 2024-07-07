45816e26fde7a7999d0d6195ca5b62d92a624a97
Saman Khalife <89409392+SamanKhalife@users.noreply.github.com>
2024-07-07 21:56:15 +0330

    Update lcd-advertizments.c

--- 11-Lcd-Advertizments/lcd-advertizments.c
+++ 11-Lcd-Advertizments/lcd-advertizments.c
@@ -1,5 +1,24 @@
 #include <mega32.h>
 #include <delay.h>
+#include <lcd.h>
 #define XTAL 8000000
-#flash char digit[9]={0x3f,}
+#asm
+equ_lcd_port = 0x18
+#end asm
+DDRA = 0xff;
+int main(){
+  lcd_init(40);
+  lcd_clear();
+  lcd_gotoxy(0,0);
+  lcd_puts("confectionery");
+  lcd_gotoxy(1,0);
+  lcd_puts("+8974564987");  
+  delay_ms(10000);
+  lcd_clear();
+  lcd_gotoxy(0,0);
+  lcd_gotoxy(1,0);
+  lcd_puts("Orders are accepted");   
+}
 
+
+  
