1b9a9b8cf871a05b47904a379496d0ea073f8fc6
Saman Khalife <89409392+SamanKhalife@users.noreply.github.com>
2024-07-07 21:52:07 +0330

    Update lcd-prints.c

--- 10-Lcd-Prints/lcd-prints.c
+++ 10-Lcd-Prints/lcd-prints.c
@@ -1,5 +1,13 @@
 #include <mega32.h>
-#include <delay.h>
+#include <lcd.h>
 #define XTAL 8000000
-#flash char digit[9]={0x3f,}
-
+#asm
+equ_lcd_port = 0x18
+#end asm
+DDRA = 0xff;
+int main(){
+  lcd_init(20);
+  lcd_clear();
+  lcd_gotoxy(0,0);
+  lcd_puts("AVR CLASS")
+}
