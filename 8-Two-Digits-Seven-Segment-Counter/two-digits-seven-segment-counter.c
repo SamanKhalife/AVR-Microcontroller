d1b4d8797d727092ffd6984e45fe4eed59c885f7
Saman Khalife <89409392+SamanKhalife@users.noreply.github.com>
2024-07-07 21:46:41 +0330

    Update two-digits-seven-segment-counter.c

--- 8-Two-Digits-Seven-Segment-Counter/two-digits-seven-segment-counter.c
+++ 8-Two-Digits-Seven-Segment-Counter/two-digits-seven-segment-counter.c
@@ -2,4 +2,15 @@
 #include <delay.h>
 #define XTAL 8000000
 #flash char digit[9]={0x3f,}
-
+DDRA = 0xff;
+DDRB = 0xff;
+int main(){
+  for(int i=0;i<=9;i++){
+    for(int j=0;j<=9;j++){
+     portB = oxo1;
+     portA = digit[i];
+     portB = oxo2;
+     portA = digit[i];
+    } 
+  }
+}
