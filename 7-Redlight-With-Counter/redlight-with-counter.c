b4deb060490ccb75de80a835f8ccd3b99df66677
Saman Khalife <89409392+SamanKhalife@users.noreply.github.com>
2024-07-07 21:42:25 +0330

    Update redlight-with-counter.c

--- 7-Redlight-With-Counter/redlight-with-counter.c
+++ 7-Redlight-With-Counter/redlight-with-counter.c
@@ -2,23 +2,29 @@
 #include <delay.h>
 #define XTAL 8000000
 #flash char digit[9]={0x3f,}
-DDRA = 0x00;
-DDRC = 0xff;
-DDRB = 0xff;
-portC = 0xff;
+DDRA = 0x3c;
+pinA.0=1;
+pinA.7=1;
 int main (){
-if(port==oxff){
+pinb.2=1;
+pinb.3=1;
+pinb.4=1;
+pinb.7=1;
+if(port d == 1){
   for (int i=0;i<=5, i++)
     {
       portc = digit[i];
       delay-ms(10000);
     }
+    pinb.2=1;
     pinb.5=1;
-    pinb.6=1;
   }
-  else
-  {
-    pinb.7=1;
-    pinb.0=1;
+  if(port c == 1){
+    for (int i=0;i<=5, i++){
+    portc = digit[i];
+    delay-ms(10000);
+    }
+    pinb.4=1;
+    pinb.3=1;
   }
 }
